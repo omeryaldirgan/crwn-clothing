a8d6c3e4592a1f3658bd9dc7d7d2a1e67659c66d
Ömer Yaldırgan <omeryaldirgan@hotmail.com>
2020-05-23 00:24:39 +0300

    with router eklendi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import {Route,Switch} from 'react-router-dom';
 
+const hatsPage=()=>(
+  <div>Hats Page</div>
+)
 function App() {
   return (
     <div>
-        <HomePage/>
+     <Switch>
+       <Route exact path='/' component={HomePage}/>
+       <Route path='/shop/hats' component={hatsPage}/>
+     </Switch>
     </div>
   );
 }
